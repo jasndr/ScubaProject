9f3d9897e296b05d425746daffbe0a83d78afc31
Jason Delos Reyes <jasndr@gmail.com>
2014-10-20 18:37:56 -1000

    New Line
    To add disclaimer.

--- ScubaAssignment/src/HelloWorld.java
+++ ScubaAssignment/src/HelloWorld.java
@@ -2,6 +2,7 @@
 public class HelloWorld {
  public static void main (String[] args){
 	 
+	 System.out.println("DISCLAIMER:  NOT TO BE USED ON REAL DIVES!");
 	 System.out.println("Hello World!");
 	 
  }
