682c709e6e215e90a126886fb55cad0e97cb4b15
Uninstall <knaka@hawaii.edu>
2014-11-07 16:02:34 -1000

    An interface which will contain final values on the table
    Added really rough comment functions to the main to show what it probably should do

---
+++ ScubaAssignment/src/FinalValues.java
@@ -0,0 +1,8 @@
+
+public interface FinalValues {
+
+	//The dive table for seeing the amount of residual nitrogen based off of pressure group and max depth
+	public final int[][] table = {{10, 19, 25, 29, 32, 36, 40, 44, 48, 52, 57, 62, 67, 73, 79, 85, 92, 100, 108, 117, 127, 139, 152, 168, 188, 205},
+									{9, 16, 22, 25, 27, 31, 34, 37, 40, 44, 48, 51, 55, 60, 64, 69, 74, 79, 85, 91, 97, 104, 111, 120, 129, 140},
+									{7, 13, 17, 19, 21, 24, 26, 28, 31, 33, 36, 39, 41, 44, 47, 50, 53, 57, 60, 63, 67, 71, 75, 80, null, null}};
+}
--- ScubaAssignment/src/HelloWorld.java
+++ ScubaAssignment/src/HelloWorld.java
@@ -2,11 +2,23 @@
 public class HelloWorld {
  public static void main (String[] args){
 	 
+	 int rNitrogen = 0;
+	 int maxDepth = 0;
+	 int diveTime = 0;
+	 char pGroup = null;
+	 char newpGroup = null;
+	 
 	 System.out.println("DISCLAIMER:  NOT TO BE USED ON REAL DIVES!");
 	 System.out.println("Hello World!");
 	 
 	 //Testing
-	 //Kris testing
-	 
+	 //Kris testing.  For example if user enters max dive as 35 and dive time as 37, we should do something like
+	 //some function that rounds up the user's max depth to the next max depth up to 140feet
+	 //if(first dive) {
+	 //    pGroup = function that finds the pressure group letter based off of rounding the user entered divetime;
+	 //}
+	 //else {
+	 //    newpGroup = FinalValues.tablethatdoesntexistyet[pGroup][dive interval];
+	 //}
  }
 }
