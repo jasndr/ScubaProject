2a923d199078342680c2cc52dfa04e7125d99175
Uninstall <knaka@hawaii.edu>
2014-11-08 02:10:30 -1000

    Added more values, functions to the main menu. THEY ARE JUST IDEAS ON HOW I THINK OUR CODE WORKS.  DO NOT TRUST IT TO WORK.

--- ScubaAssignment/src/FinalValues.java
+++ ScubaAssignment/src/FinalValues.java
@@ -1,8 +1,23 @@
 
 public interface FinalValues {
 
+	//Alphabet to let the program know which letter equals which array number
+	public final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	//Max depth number to compare user input as well as knowing which depth equals which array number
+	public final int[] maxDepth = {35, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140};
+	
 	//The dive table for seeing the amount of residual nitrogen based off of pressure group and max depth
-	public final int[][] table = {{10, 19, 25, 29, 32, 36, 40, 44, 48, 52, 57, 62, 67, 73, 79, 85, 92, 100, 108, 117, 127, 139, 152, 168, 188, 205},
+	public final int[][] nitrogenTable = {{10, 19, 25, 29, 32, 36, 40, 44, 48, 52, 57, 62, 67, 73, 79, 85, 92, 100, 108, 117, 127, 139, 152, 168, 188, 205},
 									{9, 16, 22, 25, 27, 31, 34, 37, 40, 44, 48, 51, 55, 60, 64, 69, 74, 79, 85, 91, 97, 104, 111, 120, 129, 140},
-									{7, 13, 17, 19, 21, 24, 26, 28, 31, 33, 36, 39, 41, 44, 47, 50, 53, 57, 60, 63, 67, 71, 75, 80, null, null}};
+									{7, 13, 17, 19, 21, 24, 26, 28, 31, 33, 36, 39, 41, 44, 47, 50, 53, 57, 60, 63, 67, 71, 75, 80, null, null},
+									{6, 11, 14, 16, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 42, 44, 47, 49, 52, 54, 55, null, null, null},
+									{5, 9, 12, 13, 15, 16, 18, 19, 21, 22, 24, 26, 27, 29, 31, 33, 35, 36, 38, 40, null, null, null, null, null, null},
+									{4, 8, 10, 11, 13, 14, 15, 17, 18, 19, 21, 22, 23, 25, 26, 28, 29, 30, null, null, null, null, null, null, null, null},
+									{4}};
+
+	//Dive interval portion of the table in minutes
+	public final int[][] intervalTable = {{180},
+											{47, 228}};
+	}
 }
--- ScubaAssignment/src/HelloWorld.java
+++ ScubaAssignment/src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	 
 	 int rNitrogen = 0;
 	 int maxDepth = 0;
+	 int maxDepthIndex = -1;
 	 int diveTime = 0;
+	 int pGroupIndex = -1;
 	 char pGroup = null;
 	 char newpGroup = null;
 	 
@@ -14,11 +16,30 @@ public class HelloWorld {
 	 //Testing
 	 //Kris testing.  For example if user enters max dive as 35 and dive time as 37, we should do something like
 	 //some function that rounds up the user's max depth to the next max depth up to 140feet
-	 //if(first dive) {
-	 //    pGroup = function that finds the pressure group letter based off of rounding the user entered divetime;
+	 //Does this whole thing go into a while loop?  I'm not sure how GUI code for menus work with code...
+	 //
+	 //
+	 //for(int i = 0; i < 12; i++) {
+	 //    if(FinalValues.maxDepth[i] <= maxDepth) {
+	 //        maxDepthIndex = i;
+	 //if(maxDepthIndex > 0) {
+	 //    if(first dive) {
+	 //        for(int i = 0; i < 26; i++) {
+	 //            if(FinalValues.nitrogenTable[maxDepthIndex][i] <= diveTime) {
+	 //                pGroupIndex = i;
+     //            }
+	 //			   else
+	 //                System.out.println("Invalid input or max time is too long.");
+	 //            }
+	 //        }
+	 //    }
+	 //    else {
+	 //        newpGroup = FinalValues.tablethatdoesntexistyet[pGroup][dive interval];
+	 //    }
 	 //}
 	 //else {
-	 //    newpGroup = FinalValues.tablethatdoesntexistyet[pGroup][dive interval];
+	 //    System.out.println("Invalid input or max depth is too deep (max is 140 feet/42.6 meters)");
+	 //    System.out.println("Please reinput a valid depth.");
 	 //}
  }
 }
