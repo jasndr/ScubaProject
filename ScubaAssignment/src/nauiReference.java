899cbd9be2e188d088d50319898192553a409ba7
mrenon <mrenon@hawaii.edu>
2014-12-02 21:12:19 -1000

    naui dive table picture
    just a JFrame that holds the NAUI Dive Table picture as a reference
    guide when entering data into the diveTable.
    opens when reference button on mainHub.java is clicked

---
+++ ScubaAssignment/src/nauiReference.java
@@ -0,0 +1,37 @@
+
+import java.awt.Container;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+public class nauiReference extends JFrame{
+	
+	private ImageIcon image;
+	private JLabel imageHolder;
+	private Container container;
+	
+	public nauiReference(){		
+		
+		setTitle("This program is a prototype and CANNOT be used for ACTUAL DIVES.");
+		
+		image = new ImageIcon(getClass().getResource("Dive_tables_NAUI.jpg"));
+		imageHolder = new JLabel(image);
+		container = getContentPane();
+		container.add(imageHolder);	
+		
+	}
+	
+	//if you would just like to test the diveTable
+	//comment out other main methods in other class
+/*	public static void main(String[] args){
+		nauiReference naui = new nauiReference();
+		naui.setVisible(true);
+		naui.setTitle("PROTOTYPE");
+		naui.setLocation(700,100);
+		naui.pack();
+		naui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}*/
+
+}
