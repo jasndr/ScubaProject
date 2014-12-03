603449c1909404294f4d2e5a0674e7f3769c8b8d
Uninstall <knaka@hawaii.edu>
2014-12-03 05:54:56 -1000

    fixed value to 40 from 39.

--- ScubaAssignment/src/FinalValues.java
+++ ScubaAssignment/src/FinalValues.java
@@ -6,7 +6,7 @@ public interface FinalValues {
 	public final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
 
 	//Max depth number to compare user input as well as knowing which depth equals which array number
-	public final int[] maxDepth = {12, 15, 18, 21, 24, 27, 30, 33, 36, 39	};
+	public final int[] maxDepth = {12, 15, 18, 21, 24, 27, 30, 33, 36, 40};
 	
 	//The dive table for seeing the amount dive time based off of pressure group and max depth
 	public final int[][] timeTable = {{5, 15,  25, 30, 40, 50, 70, 80, 100, 110, 130, 150}, //12 meters
