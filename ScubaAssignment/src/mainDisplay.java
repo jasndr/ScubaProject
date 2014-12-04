3ebc79c396c8dff8bbe28ec48001b4858f2855de
mrenon <mrenon@hawaii.edu>
2014-12-04 13:54:27 -1000

    main display placeholder
    pops open the image of the main display placeholder

--- ScubaAssignment/src/mainDisplay.java
+++ ScubaAssignment/src/mainDisplay.java
@@ -1,10 +1,12 @@
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -13,15 +15,19 @@ import javax.swing.SwingConstants;
 
 public class mainDisplay extends JFrame implements ActionListener{
 
-	private JLabel lbl;
+	private ImageIcon image;
+	private JLabel imageHolder;
+	private Container container;
 	
 	public mainDisplay(final JButton button, final JButton button2, final JButton button3, final JButton button4){
 		setTitle("This program is a prototype and CANNOT be used for ACTUAL DIVES.");
 	
-		lbl = new JLabel("Placeholder: The MAIN DISPLAY GUI isn't implemented yet", SwingConstants.CENTER);
-		lbl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		add(lbl);
-		
+		//get the image from the default package
+		image = new ImageIcon(getClass().getResource("mainDis_img.jpg"));
+		imageHolder = new JLabel(image); //set the image to a label
+		container = getContentPane();
+		container.add(imageHolder);	 //add the image to a container
+		 
 		
 		//add event listener
 		addWindowListener(new WindowAdapter(){
