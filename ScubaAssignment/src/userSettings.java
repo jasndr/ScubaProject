0f5403d31a4c53f08f629263d935f9f6269dbc1f
mrenon <mrenon@hawaii.edu>
2014-12-03 16:25:02 -1000

    settings GUI
    just a place holder GUI that tells the user its a place holder

---
+++ ScubaAssignment/src/userSettings.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+
+public class userSettings extends JFrame implements ActionListener{
+
+	private JLabel lbl;
+	
+	public userSettings(final JButton button){
+		setTitle("This program is a prototype and CANNOT be used for ACTUAL DIVES.");
+	
+		lbl = new JLabel("Placeholder: The SETTINGS GUI isn't implemented yet", SwingConstants.CENTER);
+		lbl.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		add(lbl);
+		
+		
+		//add event listener
+		addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				button.setEnabled(true);
+			}
+		});
+		
+	}
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
